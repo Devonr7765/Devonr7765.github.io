32f192614141e85cca4e86f3ff15d0c35b2a6e84
Devonr7765 <Devonr@saota.com>
2017-03-07 15:41:34 +0200

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -32,6 +32,7 @@ $("#pyramidDiv").mousemove(function (e) {
         x: e.pageX - offset.left,
         y: e.pageY - offset.top
     };
+
     var xPercent = divPos.x / $("#pyramidDiv").width() * 100;
 
     if (xPercent > 0 && xPercent < 25) {
@@ -51,4 +52,37 @@ $("#pyramidDiv").mousemove(function (e) {
 }).mouseout( function () {
     $('#HoverImg').attr('src', null);
 });
+
+
+$("#products").click(function () {
+    if ($(this).attr('href') == "#LuxWalker") {
+        $(this).attr('href', '#LuxViewer')
+    }
+    else {
+
+        $(this).attr('href', '#LuxWalker')
+    }
+
+});
+$('.tile')
+  // tile mouse actions
+  .on('mouseover', function () {
+      $(this).children('.photo').css({ 'transform': 'scale(' + $(this).attr('data-scale') + ')' });
+  })
+  .on('mouseout', function () {
+      $(this).children('.photo').css({ 'transform': 'scale(1)' });
+  })
+  .on('mousemove', function (e) {
+      $(this).children('.photo').css({ 'transform-origin': ((e.pageX - $(this).offset().left) / $(this).width()) * 100 + '% ' + ((e.pageY - $(this).offset().top) / $(this).height()) * 100 + '%' });
+  })
+  // tiles set up
+  .each(function () {
+      $(this)
+        // add a photo container
+        .append('<div class="photo"></div>')
+        // some text just to show zoom level on current item in this example
+        .append('<div class="txt"><div class="x">' + $(this).attr('data-scale') + 'x</div>ZOOM ON<br>HOVER</div>')
+        // set up a background image for each tile based on data-image attribute
+        .children('.photo').css({ 'background-image': 'url(' + $(this).attr('data-image') + ')' });
+  })
 
